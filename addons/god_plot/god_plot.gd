0ead6896e9986d9b2ce5f084f6bcdc51b45ef1bb
Omar - Home <negmok@gmail.com>
2024-11-12 15:46:29 -0500

    Update series node icons

--- addons/god_plot/god_plot.gd
+++ addons/god_plot/god_plot.gd
@@ -3,10 +3,10 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_custom_type("Graph2D", "Control", preload("tools/graphs/graph_2d.gd"), preload("icon.svg"))
-	add_custom_type("ScatterSeries", "Node", preload("tools/series/series_types/scatter_series.gd"), preload("icon.svg"))
-	add_custom_type("LineSeries", "Node", preload("tools/series/series_types/line_series.gd"), preload("icon.svg"))
-	add_custom_type("AreaSeries", "Node", preload("tools/series/series_types/area_series.gd"), preload("icon.svg"))
+	add_custom_type("Graph2D", "Control", preload("tools/graphs/graph_2d.gd"), preload("icons/icon.svg"))
+	add_custom_type("ScatterSeries", "Node", preload("tools/series/series_types/scatter_series.gd"), preload("icons/scatter_icon.svg"))
+	add_custom_type("LineSeries", "Node", preload("tools/series/series_types/line_series.gd"), preload("icons/line_icon.svg"))
+	add_custom_type("AreaSeries", "Node", preload("tools/series/series_types/area_series.gd"), preload("icons/area_icon.svg"))
 
 
 func _exit_tree() -> void:
